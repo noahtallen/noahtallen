a7d30cd22ebaceab5f290fa1205518b4c186cea2
Noah Allen <noahtallen@gmail.com>
2017-07-22 19:59:59 -0400

    All hover events are now disabled for touch devices.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,6 +6,23 @@ var startEvent = isTouchSupported ? 'touchstart' : 'mousedown';
 var moveEvent = isTouchSupported ? 'touchmove' : 'mousemove';
 var endEvent = isTouchSupported ? 'touchend' : 'mouseup';
 
+if (isTouchSupported) { // remove all :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+}
+
 init();
 
 function init() {
