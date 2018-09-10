6ba03f0f88b61264339b2c2e3c3765086de4ae7c
Noah Allen <noahtallen@gmail.com>
2018-09-10 14:49:07 -0400

    setting up typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Noah Allen 👨‍💻',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.js'
+      }
+    }
+  ],
 }
