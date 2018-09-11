a2d75115a5dfc69d719ea46d74bf3def8fe4d570
Noah Allen <noahtallen@gmail.com>
2018-09-11 15:02:32 -0400

    Add interactive company name

---
+++ src/scripts/script.js
@@ -0,0 +1,7 @@
+function changeCompanyName (e) {
+    var name = e.target.value
+    // Set each input:
+    document.getElementById('comp1').value = name
+    document.getElementById('comp2').value = name
+    document.getElementById('comp3').value = name
+}
