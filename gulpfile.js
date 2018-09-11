ad41b9d08c9d1b0aa6dd96c8c255a2c2446307a0
Noah Allen <noahtallen@gmail.com>
2018-09-11 12:32:02 -0400

    add robots.txt

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('copyImages', () => {
 })
 
 gulp.task('copyRootFiles', () => {
-    return gulp.src(['src/favicon.ico', 'src/styles/circular-std.woff', 'src/styles/inconsolata.ttf']).pipe(gulp.dest('public/'))
+    return gulp.src(['src/favicon.ico', 'src/styles/circular-std.woff', 'src/styles/inconsolata.ttf', 'src/robots.txt']).pipe(gulp.dest('public/'))
 })
 
 gulp.task('compile', ['compileCss', 'copyImages', 'copyRootFiles'])
