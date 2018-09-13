b5ce794ce1b7a084141b6018ab35b6e8fa8f7711
Noah Allen <noahtallen@gmail.com>
2018-09-13 18:41:00 -0400

    fix validation issues

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,67 +1,65 @@
 function init () {
-    const ul = document.getElementById('info-items')
+    var ul = document.getElementById("info-items");
     if (ul) {
-        var lis = ul.getElementsByTagName('li')
-        for (let i = 0; i < lis.length; i++) {
-            const li = lis.item(i)
-            li.addEventListener('click', (e) => {
-                showSubpage(e)
+        var lis = ul.getElementsByTagName("li");
+        for (var i = 0; i < lis.length; i++) {
+            var li = lis.item(i);
+            li.addEventListener("click", function (e) {
+                showSubpage(e);
             })
         }
     }
 }
 
 function changeCompanyName (e) {
-    var name = e.target.value
+    var name = e.target.value;
     // Set each input:
-    document.getElementById('comp1').value = name
-    document.getElementById('comp2').value = name
-    document.getElementById('comp3').value = name
+    document.getElementById("comp1").value = name;
+    document.getElementById("comp2").value = name;
+    document.getElementById("comp3").value = name;
 }
 
 function showSubpage (event) {
     // Get ID of info div:
-    const targetID = event.target.dataset.target
+    var targetID = event.target.dataset.target;
     // Show subpage & inf0 div:
-    const sub = document.getElementById(targetID)
-    sub.style.display = 'block'
-    sub.style.zIndex = 10
-    setTimeout(() => {
-        sub.style.opacity = 1
+    var sub = document.getElementById(targetID);
+    sub.style.display = "block";
+    sub.style.zIndex = 10;
+    setTimeout(function () {
+        sub.style.opacity = 1;
     }, 20)
 
     // Hide main page:
-    const main = document.getElementById('main-page')
-    main.style.zIndex = -10
-    main.style.opacity = 0
-    setTimeout(() => {
-        main.style.display = 'none'
+    var main = document.getElementById("main-page");
+    main.style.zIndex = -10;
+    main.style.opacity = 0;
+    setTimeout(function () {
+        main.style.display = "none";
     }, 500)
-   
 }
 
 function closeSubpage (e) {
     // Show main page:
-    const main = document.getElementById('main-page')
-    main.style.display = 'block'
-    main.style.zIndex = 10
-    setTimeout(() => {
-        main.style.opacity = 1
+    var main = document.getElementById("main-page");
+    main.style.display = "block";
+    main.style.zIndex = 10;
+    setTimeout(function () {
+        main.style.opacity = 1;
     }, 20)
 
     // Hide subpage:
-    const sub = e.target.parentNode.parentNode
-    console.log(sub)
-    sub.style.zIndex = -10
-    sub.style.opacity = 0
-    setTimeout(() => {
-        sub.style.display = 'none'
+    var sub = e.target.parentNode.parentNode;
+    sub.style.zIndex = -10;
+    sub.style.opacity = 0;
+    setTimeout(function () {
+        sub.style.display = "none";
     }, 500)
 
-    const sections = document.getElementsByClassName('info-section')
+    var sections = document.getElementsByClassName("info-section");
     if (sections) {
-        for (let i = 0; i < sections.length; i++) {
-            sections[i].style.display = 'none'
+        for (var i = 0; i < sections.length; i++) {
+            sections[i].style.display = "none";
         }
     }
 }
